1fac9e29832c245d989a23596b504bea26f385cb
Rskhm (AMD) <ann.mauduy.decius@gmail.com>
2024-08-27 16:30:41 -0400

    update generator

--- support-scripts/agendas/framework/__init__.py
+++ support-scripts/agendas/framework/__init__.py
@@ -1,15 +1,12 @@
 from agendas.framework.Agenda import Agenda, EffectRecurrence, Category
-from agendas.framework.PdxObject import PdxObject
-from agendas.framework.generator import PdxObjectList
+from agendas.framework.generator import Generator
 
 def main():
     institutional_exploitation = Agenda("institutional_exploitation", 0)
     ideological_grifting = Agenda("ideological_grifting", 1)
-    institutional_exploitation.add_effect({"set_country_flag": "bpm_institutional_exploitation"}, EffectRecurrence.AGENDA_TICK)
-    ideological_grifting.add_effect({"set_country_flag": "bpm_ideological_grifting"}, EffectRecurrence.AGENDA_TICK)
+    institutional_exploitation.add_effect({"set_variable": "bpm_institutional_exploitation"}, EffectRecurrence.AGENDA_TICK)
+    ideological_grifting.add_effect({"set_variable": "bpm_ideological_grifting"}, EffectRecurrence.AGENDA_TICK)
     exploitation = Category("exploitation", [institutional_exploitation, ideological_grifting])
 
-    obj = PdxObjectList()
-    obj.generate_effects([exploitation])
-    obj.generate_picker([exploitation])
-    obj.write_to_files()
+    obj = Generator()
+    obj.generate([exploitation])
--- support-scripts/agendas/framework/generator.py
+++ support-scripts/agendas/framework/generator.py
@@ -1,153 +1,158 @@
+import os
+import shutil
 from agendas.framework.Agenda import Agenda, EffectRecurrence, Category
 from agendas.framework.PdxObject import PdxObject
 
-# generate effects
-# generate picker (the picker will also call the on_add effect if applicable)
-# generate script value consts
 
-# The picker needs to generate a script value for each category
-# Then it generates the scripted effect
-def generate_picker(categories: list[Category]):
-    script_values = {}
-    scripted_effects = {}
-
-    total_weight_list = []
-
-    effect_temp = []
-    for category in categories:
-        script_values[category.script_value_name()] = category.script_value()
-        script_values[category.script_value_name_normalized()] = [
-            {"value": 0},
-            {"add": category.script_value_name()},
-            {"divide": "bpm_agenda_categories_total_weight"},
-            {"multiply": 100},
-        ]
-        total_weight_list.append({"add": category.script_value_name()})
-        effect_temp.append({
-            "change_variable": [
-                { "name": "bpm_agenda_category_weight_running_total" },
-                {
-                    "add": category.script_value_name_normalized()
-                }
-            ]
-        })
-        effect_temp.append(
-            {
-                "if": [
-                    {
-                        "limit": [
-                            "var:bpm_agenda_category_random_number <= var:bpm_agenda_category_weight_running_total",
-                            { "NOT": {
-                                "has_variable": "bpm_agenda_picker_ifelse_ended"
-                            } }
-                        ]
-                        
-                    },
-                    "# do something here",
-                    { "set_variable": "bpm_agenda_picker_ifelse_ended" }
-                ]
-            }
-        )
-
-    script_values["bpm_agenda_categories_total_weight"] = [{"value": 0}] + total_weight_list
-
-
-    final_scripted_effect = [ 
-        {
-            "set_variable": [
-                { "name": "bpm_agenda_category_random_number" },
-                {
-                    "value": {
-                        "integer_range": {
-                            "min": 0,
-                            "max": 100
-                        }
-                    }
-                }
-            ]
-        },
-        {
-            "set_variable": [
-                { "name": "bpm_agenda_category_weight_running_total" },
-                {
-                    "value": 0
-                }
-            ]
-        }
-    ] + effect_temp
-
-    scripted_effects["bpm_agenda_picker"] = final_scripted_effect
-
-    return {
-        "scripted_effects": [PdxObject(scripted_effects)],
-        "script_values": [PdxObject(script_values)]
-    }
+FOLDER = "support-scripts/agendas/framework-output"
+class Generator:
     
-
-def generate_effects(categories: list[Category]):
-    a = generate_effect_for_tick(categories, EffectRecurrence.AGENDA_TICK)
-    b = generate_effect_for_tick(categories, EffectRecurrence.MONTHLY)
-    return {
-        "scripted_effects": [a[1], b[1]],
-        "on_actions": [a[0], b[0]]
-    }
-
-def generate_effect_for_tick(categories: list[Category], tick: EffectRecurrence):
-    agendas = [ agenda for category in categories for agenda in category.agendas ]
-    scripted_effects = {}
-    caller_list = []
-    for agenda in agendas:
-        if agenda.has_tick_effect(tick):
-            scripted_effects[agenda.scripted_effect_name(tick)] = agenda.scripted_effect(tick)
-            #print(agenda.scripted_effect(EffectRecurrence.AGENDA_TICK))
-            caller_list.append(
-                {
-                    "if": [
-                        {
-                            "limit": {
-                                "bpm_has_agenda": agenda.id
-                            }
-                        },
-                        {
-                            agenda.scripted_effect_name(tick): True
-                        }
-                    ],
-                }
-            )
-    raw_on_action = [
-        { 
-            tick.get_pulse(): {
-                "on_actions": [
-                    f"on_bpm_{tick}_tick"
-                ]
-            }
-        },
-        { f"on_bpm_{tick}_tick": caller_list }
-    ]
-
-    return ( PdxObject(raw_on_action), PdxObject(scripted_effects) ) 
-
-
-
-class PdxObjectList:
-
     def __init__(self):
         self.scripted_effects = []
         self.on_actions = []
         self.scripted_triggers = []
         self.script_values = []
 
-    def __generate(self, f, categories):
-        for k, v in f(categories).items():
-            setattr(self, k, v)
+    def generate(self, categories):
+        self.generate_effects(categories)
+        self.generate_picker(categories)
+        self.write_to_files()
 
-    def write_to_files(self):
-        for k, v in self.__dict__.items():
-            with open(f"support-scripts/agendas/framework-output/{k}.txt", "w") as f:
-                f.write('\n\n'.join(str(x) for x in v))
+    def generate_picker(self, categories):
+        script_values = self.__generate_script_values(categories)
+        effect_templates = self.__create_effect_templates(categories)
+        final_scripted_effect = self.__assemble_final_scripted_effect(effect_templates)
+
+        scripted_effects = {
+            "bpm_agenda_picker": final_scripted_effect
+        }
+
+        self.scripted_effects.append(PdxObject(scripted_effects))
+        self.script_values.append(PdxObject(script_values))
+
+    def __generate_script_values(self, categories):
+        script_values = {}
+        total_weight_list = []
+
+        for category in categories:
+            script_values[category.script_value_name()] = category.script_value()
+            script_values[category.script_value_name_normalized()] = [
+                {"value": 0},
+                {"add": category.script_value_name()},
+                {"divide": "bpm_agenda_categories_total_weight"},
+                {"multiply": 100},
+            ]
+            total_weight_list.append({"add": category.script_value_name()})
+
+        script_values["bpm_agenda_categories_total_weight"] = [{"value": 0}] + total_weight_list
+        
+        return script_values
+
+    def __create_effect_templates(self, categories):
+        effect_templates = []
+
+        for category in categories:
+            effect_templates.append({
+                "change_variable": [
+                    {"name": "bpm_agenda_category_weight_running_total"},
+                    {"add": category.script_value_name_normalized()}
+                ]
+            })
+            effect_templates.append({
+                "if": [
+                    {
+                        "limit": [
+                            "var:bpm_agenda_category_random_number <= var:bpm_agenda_category_weight_running_total",
+                            {"NOT": {"has_variable": "bpm_agenda_picker_ifelse_ended"}}
+                        ]
+                    },
+                    "# do something here",
+                    {"set_variable": "bpm_agenda_picker_ifelse_ended"}
+                ]
+            })
+
+        return effect_templates
+
+    def __assemble_final_scripted_effect(self, effect_templates):
+        final_scripted_effect = [
+            {
+                "set_variable": [
+                    {"name": "bpm_agenda_category_random_number"},
+                    {
+                        "value": {
+                            "integer_range": {
+                                "min": 0,
+                                "max": 100
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                "set_variable": [
+                    {"name": "bpm_agenda_category_weight_running_total"},
+                    {"value": 0}
+                ]
+            }
+        ] + effect_templates
+        
+        return final_scripted_effect
 
     def generate_effects(self, categories):
-        self.__generate(generate_effects, categories)
-    
-    def generate_picker(self, categories):
-        self.__generate(generate_picker, categories)
+        self.__generate_effect_for_tick(categories, EffectRecurrence.AGENDA_TICK)
+        self.__generate_effect_for_tick(categories, EffectRecurrence.MONTHLY)
+
+    def __generate_effect_for_tick(self, categories, tick):
+        agendas = [agenda for category in categories for agenda in category.agendas]
+        scripted_effects = {}
+        caller_list = []
+        for agenda in agendas:
+            if agenda.has_tick_effect(tick):
+                scripted_effects[agenda.scripted_effect_name(tick)] = agenda.scripted_effect(tick)
+                caller_list.append(
+                    {
+                        "if": [
+                            {
+                                "limit": {
+                                    "bpm_has_agenda": agenda.id
+                                }
+                            },
+                            {
+                                agenda.scripted_effect_name(tick): True
+                            }
+                        ],
+                    }
+                )
+        raw_on_action = [
+            { 
+                tick.get_pulse(): {
+                    "on_actions": [
+                        f"on_bpm_{tick}_tick"
+                    ]
+                }
+            },
+            { f"on_bpm_{tick}_tick": caller_list }
+        ]
+
+        self.on_actions.append(PdxObject(raw_on_action))
+        self.scripted_effects.append(PdxObject(scripted_effects))
+
+    def delete_all_files_in_folder(self, folder_path):
+        if os.path.exists(folder_path) and os.path.isdir(folder_path):
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)  
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path) 
+                except Exception as e:
+                    print(f"Failed to delete {file_path}. Reason: {e}")
+        else:
+            print(f"The folder {folder_path} does not exist or is not a directory.")
+
+    def write_to_files(self):
+        self.delete_all_files_in_folder(FOLDER)
+        for k, v in self.__dict__.items():
+            with open(f"{FOLDER}/{k}.txt", "w") as f:
+                f.write('\n'.join(str(x) for x in v))

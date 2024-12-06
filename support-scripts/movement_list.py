cccfba4e0d54ccdd2b7bb31262d6b29ee4c78098
Rskhm (AMD) <ann.mauduy.decius@gmail.com>
2024-12-06 18:02:41 -0500

    implemented liberal, modernizer and radical splits

--- support-scripts/movement_list.py
+++ support-scripts/movement_list.py
@@ -2,6 +2,7 @@ mod = 'better-politics-mod/common/political_movements/'
 game = 'C:/Steam/steamapps/common/Victoria 3/game/common/political_movements/' 
 import os
 import re 
+import pdxpy
 r = re.compile(r'movement_(.*?) = \{')
 def process_file(f):
     with open(f, 'r', encoding='utf-8') as file:
@@ -19,4 +20,54 @@ mod_files = [mod + file for file in os.listdir(mod)]
 game_files = [game + file for file in os.listdir(game)]
 result = list(set(list(flatten_map(process_file, mod_files)) + list(flatten_map(process_file, game_files))))
 
-print(result)
+
+def create_setter(result):
+    s = [] 
+    n = False
+    for k in result:
+        if not n:
+            s.append(
+                pdxpy.PdxUtil.if_statement(
+                    {"scope:polmov": [f"is_political_movement_type = movement_{k}"]},
+                    pdxpy.PdxUtil.set_variable(f"bpm_movement_{k}_pressure", "scope:result")
+                )
+            )
+        else:
+            s.append(
+                {"else_if": pdxpy.PdxUtil.if_statement(
+                    {"scope:polmov": [f"is_political_movement_type = movement_{k}"]},
+                    pdxpy.PdxUtil.set_variable(f"bpm_movement_{k}_pressure", "scope:result")
+                )["if"]}
+            )
+        n = True
+    return {"bpm_movement_setter": s}
+
+
+def create_getter(result):
+    s = [] 
+    n = False
+    for k in result:
+        if not n:
+            s.append(
+                pdxpy.PdxUtil.if_statement(
+                    {"scope:polmov": [f"is_political_movement_type = movement_{k}"]},
+                    pdxpy.PdxUtil.set_variable(f"bpm_movement_pressure_getter_result", f"var:bpm_movement_{k}_pressure")
+                )
+            )
+        else:
+            s.append(
+                {"else_if": pdxpy.PdxUtil.if_statement(
+                    {"scope:polmov": [f"is_political_movement_type = movement_{k}"]},
+                    pdxpy.PdxUtil.set_variable(f"bpm_movement_pressure_getter_result", f"var:bpm_movement_{k}_pressure")
+                )["if"]}
+            )
+        n = True
+    return {"bpm_movement_getter": s}
+
+
+     
+
+with open('support-scripts/movement_list.txt', 'w', encoding='utf-8') as file:
+    file.write(str(pdxpy.PdxObject(create_setter(result))))
+    file.write('\n')
+    file.write(str(pdxpy.PdxObject(create_getter(result))))

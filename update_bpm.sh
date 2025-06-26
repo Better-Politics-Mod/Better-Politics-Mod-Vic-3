bf7f3628694678fd3cab93726cbceb9e21a69075
rskhm <ann.mauduy.decius@gmail.com>
2025-06-25 23:45:35 -0400

    Add mod ID to the command

--- update_bpm.sh
+++ update_bpm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source create-release-vdf.sh better-politics-mod steam_desc.txt &> /dev/null
+source create-release-vdf.sh better-politics-mod steam_desc.txt 2932134122 &> /dev/null
 echo $1
 steamcmd +login $1 $2 +workshop_build_item $(realpath workshop.vdf) +quit
